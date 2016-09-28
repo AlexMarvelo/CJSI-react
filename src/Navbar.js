ddde809779f6e9cf1417ac1110db4692bf6180a4
AlexMarvelo <mironenkoalexey@gmail.com>
2016-09-28 14:43:27 +0300

    small fixes

--- src/Navbar.js
+++ src/Navbar.js
@@ -13,7 +13,7 @@ export default class Navbar extends Component {
                     <span className="icon-bar"></span>
                     <span className="icon-bar"></span>
                 </button>
-                <a className="navbar-brand" href="#">MyTube</a>
+                <a className="navbar-brand" href="/">MyTube</a>
             </div>
 
             <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
